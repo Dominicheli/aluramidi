4214ad8b1b803ea4e0c696a38d9ccaf3a47d3c1f
Dominicheli <felipe.dominicheli@gmail.com>
2022-07-29 09:15:03 -0300

    feat(main): when you click on the key it emits the specific sound

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-function tocaSom(idAudio) {
-  document.querySelector(idAudio).play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
 
-while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onclick = tocaSom;
-
-  contador = contador + 1;
-
-  console.log(contador);
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
 }
