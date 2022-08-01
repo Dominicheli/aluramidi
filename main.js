d802d75a85b861bb169989f115c17153ba2346a8
Dominicheli <felipe.dominicheli@gmail.com>
2022-08-01 10:16:52 -0300

    end of the project, congratulations

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio).play();
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    alert("Elemento não encontrado ou seletor inválido");
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
@@ -11,5 +17,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
   tecla.onclick = function () {
     tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
   };
 }
